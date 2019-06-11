647870ac9ce515db57625384a76daf536e353867
Amanda Garcia <algarcian@outlook.com>
2019-06-11 11:01:01 -0600

    Added static components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import './App.css';
+import React, { Fragment } from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./components/layout/Home";
+import About from "./components/layout/About";
+import NoMatch from "./components/layout/NoMatch";
+import { Container } from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Fragment>
+    <Container style={{padding: "30px 0"}}>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route component={NoMatch} />
+      </Switch>
+    </Container>
+  </Fragment>
+);
 
 export default App;
